f90026d61aac76c13e822d05f2539669fe79cdb3
Karl Rombauts <karlrombauts@gmail.com>
2020-07-10 23:31:14 +1000

    Use common word list for command line game

--- src/main/java/view/Client.java
+++ src/main/java/view/Client.java
@@ -3,17 +3,16 @@ package view;
 import util.Dictionary;
 import model.GameEngine;
 import model.HumanPlayer;
-import model.Words;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
-        GameEngine gameEngine = new GameEngine(Words.fourLetterWords);
+        GameEngine gameEngine = new GameEngine(Dictionary.load("src/main/java/dictionaries/common-four-letter.txt"));
         gameEngine.addPlayer(new HumanPlayer("Player 1", scanner));
         gameEngine.addPlayer(new HumanPlayer("Player 2", scanner));
-
         gameEngine.start();
     }
 }
